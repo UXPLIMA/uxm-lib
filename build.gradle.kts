a133fb72ee019b4a3cd6f022cf533d312265cff5
Sirac Ozmen <siracozmen@protonmail.com>
2026-08-29 08:29:15 +0000

    build: open 0.12.0 development

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@
 
 allprojects {
     group = "com.uxplima.uxmlib"
-    version = project.findProperty("projectVersion")?.toString() ?: "0.11.0-SNAPSHOT"
+    version = project.findProperty("projectVersion")?.toString() ?: "0.12.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
